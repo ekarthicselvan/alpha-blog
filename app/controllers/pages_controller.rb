0b53bdce277b11c71bd71396ca4e25fd1e2acd3e
Karthic Selvan <ekarthic@hotmail.com>
2017-01-19 05:23:04 +0000

    Create rails application and setup pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,24 @@
+class PagesController < ApplicationController
+    def home
+    end
+    
+    def about
+    end
+    
+    def products
+    end
+    
+    def customers
+    end
+    
+    def blog
+    end
+    
+    def support
+    end
+    
+    def contact
+    end
+    
+    
+end
